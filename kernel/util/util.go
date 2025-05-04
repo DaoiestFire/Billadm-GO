5d291080fb9e917060c04f4d1afd27e2a969db70
ljw <liujingwei1997@outlook.com>
2025-05-04 22:26:57 +0800

    工具函数GetRootDir

--- kernel/util/util.go
+++ kernel/util/util.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"math/rand"
+	"os"
 	"path/filepath"
 	"runtime"
 
@@ -19,6 +20,21 @@ func GetTestDir() string {
 	_, filename, _, _ := runtime.Caller(0)
 	dir := filepath.Dir(filepath.Dir(filename))
 	return filepath.Join(dir, "test")
+}
+
+func GetRootDir() string {
+	exe, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	// 解析符号链接以获取真实路径
+	realPath, err := filepath.EvalSymlinks(exe)
+	if err != nil {
+		panic(err)
+	}
+
+	return filepath.Dir(filepath.Dir(realPath))
 }
 
 func GetRandomString(l int) string {
