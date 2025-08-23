f98ca90b0a7693d78a0c74d3253199454c605289
ljw <liujingwei1997@outlook.com>
2025-08-23 21:08:43 +0800

    消费记录表格中的消费时间还是显示全部以兼容日后可能的需求

--- app/src/components/TransactionRecordTable.vue
+++ app/src/components/TransactionRecordTable.vue
@@ -153,12 +153,7 @@ const formatCell = (field, value) => {
 const formatTime = (timestamp) => {
   if (!timestamp) return ''
   const date = new Date(timestamp * 1000)
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-
-  // 按照 %YYYY/%MM/%dd 格式拼接
-  return `${year}/${month}/${day}`;
+  return date.toLocaleString()
 }
 
 // 格式化交易类型
