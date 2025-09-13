5eaca968af5ba1a776dd78fa9a3d2624b6d25770
ljw <liujingwei1997@outlook.com>
2025-09-14 00:30:39 +0800

    增加访问后端工作空间接口的工具函数

---
+++ app/src/backend/workspace.js
@@ -0,0 +1,21 @@
+import api_client from "@/backend/api_client.js";
+import {isResponseSuccess} from "@/backend/functions.js";
+
+export async function openWorkspace(workspace) {
+    const resp = await api_client.post('/v1/workspace/open_workspace', {'workspaceDir': workspace});
+    if (!isResponseSuccess(resp)) {
+        throw "openWorkspace 响应错误"
+    }
+    return resp.code
+}
+
+export async function hasOpenedWorkspace() {
+    const resp = await api_client.post('/v1/workspace/is_opened');
+    if (!isResponseSuccess(resp)) {
+        throw "hasOpenedWorkspace 响应错误"
+    }
+    return {
+        opened: resp.code === 0,
+        workspace: resp.data,
+    }
+}
