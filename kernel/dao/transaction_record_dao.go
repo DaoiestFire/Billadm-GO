4913da2e8f8cc06abdf2376944263649acc20d59
ljw <liujingwei1997@outlook.com>
2025-09-21 15:56:22 +0800

    默认排序: 消费时间降序, 消费降序, 价格降序

--- kernel/dao/transaction_record_dao.go
+++ kernel/dao/transaction_record_dao.go
@@ -62,7 +62,7 @@ func (t *transactionRecordDaoImpl) ListAllTrByLedgerId(ws *workspace.Workspace, 
 func (t *transactionRecordDaoImpl) QueryTrsByPage(ws *workspace.Workspace, condition *dto.QueryCondition) ([]*models.TransactionRecord, error) {
 	trs := make([]*models.TransactionRecord, 0)
 	db := ws.GetDb().Where("ledger_id = ?", condition.LedgerID)
-	db = db.Order("transaction_at desc, category desc")
+	db = db.Order("transaction_at desc, category desc, price desc")
 	if condition.Offset != -1 && condition.Limit != -1 {
 		db = db.Offset(condition.Offset).Limit(condition.Limit)
 	}
