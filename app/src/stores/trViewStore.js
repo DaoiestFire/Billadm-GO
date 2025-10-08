4bdcb8ae4a1cbf7cbb7d071cc6d47ed22b7dbea4
ljw <liujingwei1997@outlook.com>
2025-10-09 01:37:34 +0800

    修复时间变化页码超出范围的问题

--- app/src/stores/trViewStore.js
+++ app/src/stores/trViewStore.js
@@ -98,7 +98,7 @@ export const useTrViewStore = defineStore('trView', () => {
         await refreshTableData();
     })
 
-    watch(() => [ledgerStore.currentLedger, LedgerpageSize.value, tsRange.value], async () => {
+    watch(() => [ledgerStore.currentLedger, pageSize.value, tsRange.value], async () => {
         resetView();
         await refreshPages();
         await refreshTableData();
