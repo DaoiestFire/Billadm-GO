49b07970306e6e4321c8121c592e46425917e1e2
ljw <liujingwei1997@outlook.com>
2025-10-10 22:57:46 +0800

    购物消费中增加还款选项 消费记录默认按照消费时间交易类型消费类型金额排序

--- kernel/dao/transaction_record_dao.go
+++ kernel/dao/transaction_record_dao.go
@@ -64,7 +64,7 @@ func (t *transactionRecordDaoImpl) ListAllTrByLedgerId(ws *workspace.Workspace, 
 func (t *transactionRecordDaoImpl) QueryTrsOnCondition(ws *workspace.Workspace, condition *dto.QueryCondition) ([]*models.TransactionRecord, error) {
 	trs := make([]*models.TransactionRecord, 0)
 	db := ws.GetDb().Where("ledger_id = ?", condition.LedgerID)
-	db = db.Order("transaction_at desc, transaction_type desc, category desc, price desc")
+	db = db.Order("transaction_at desc, transaction_type asc, category desc, price desc")
 	if condition.Offset != -1 && condition.Limit != -1 {
 		db = db.Offset(condition.Offset).Limit(condition.Limit)
 	}
@@ -99,7 +99,7 @@ func (t *transactionRecordDaoImpl) QueryPriceOnCondition(ws *workspace.Workspace
 	var price sql.NullFloat64
 	db := ws.GetDb().Model(&models.TransactionRecord{})
 	db = db.Where("ledger_id = ?", condition.LedgerID)
-	db = db.Order("transaction_at desc, category desc, price desc")
+	db = db.Order("transaction_at desc, transaction_type asc, category desc, price desc")
 	if condition.Offset != -1 && condition.Limit != -1 {
 		db = db.Offset(condition.Offset).Limit(condition.Limit)
 	}
