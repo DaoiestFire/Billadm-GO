e35af595cd896285c94ebc35470761b1e1a6a8ca
ljw <liujingwei1997@outlook.com>
2025-11-08 23:30:57 +0800

    基于ant的卡片实现图标显示

--- app/src/components/da_view/BilladmChartPanel.vue
+++ app/src/components/da_view/BilladmChartPanel.vue
@@ -1,13 +1,7 @@
 <!-- components/da_view/BilladmChartPanel.vue -->
 <template>
-  <div class="panel-outer">
-    <div class="panel-header">
-      <h3 class="chart-title">{{ title }}</h3>
-    </div>
-    <div class="panel-container">
-      <BilladmChart :option="option"/>
-    </div>
-    <div class="panel-footer">
+  <a-card :title="title" :body-style="bodyCss">
+    <template #extra>
       <a-checkbox-group
           v-if="title==='交易走势'"
           v-model:value="tradingTrendChecked"
@@ -18,18 +12,21 @@
           v-model:value="transactionTypeChecked"
           :options="transactionTypeOptions"
       />
-    </div>
-  </div>
+    </template>
+    <BilladmChart :option="option"/>
+  </a-card>
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from 'vue';
+import {computed, type CSSProperties, ref} from 'vue';
 import BilladmChart from "@/components/da_view/BilladmChart.vue";
-import {useCssVariables} from "@/backend/css.ts";
 import {buildOptionForTradingTrend, buildOptionForTransactionDistribution} from "@/backend/table.ts";
 import type {TransactionRecord} from "@/types/billadm";
 
-const {uiSizeMenuWidth} = useCssVariables();
+const bodyCss: CSSProperties = {
+  aspectRatio: 3 / 2,
+  minHeight: 0
+}
 
 const props = defineProps<{
   title: string
@@ -56,41 +53,3 @@ const option = computed(() => {
   }
 });
 </script>
-
-<style scoped>
-.panel-outer {
-  background: var(--billadm-color-major-background-color);
-  border: 1px solid var(--billadm-color-window-border-color);
-  border-radius: 8px;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  width: auto;
-  height: auto;
-}
-
-/* header & footer */
-.panel-header, .panel-footer {
-  height: v-bind(uiSizeMenuWidth);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 8px;
-}
-
-.panel-header {
-  border-bottom: 1px solid var(--billadm-color-window-border-color);
-}
-
-.panel-footer {
-  border-top: 1px solid var(--billadm-color-window-border-color);
-}
-
-.panel-container {
-  flex: 1;
-  position: relative;
-  width: 100%;
-  aspect-ratio: 3 / 2;
-  min-height: 0;
-}
-</style>
