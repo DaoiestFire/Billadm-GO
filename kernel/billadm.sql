966328c90d3fdeb4e8035ca4c22c8654899c0cd2
ljw <liujingwei1997@outlook.com>
2026-01-08 22:24:28 +0800

    解决建表错误

--- kernel/billadm.sql
+++ kernel/billadm.sql
@@ -37,25 +37,6 @@ CREATE TABLE IF NOT EXISTS tbl_billadm_category
     transaction_type TEXT NOT NULL DEFAULT ''
 );
 
-INSERT INTO tbl_billadm_category (name, scope, transaction_type)
-VALUES ('餐饮美食', 'system', 'expense'),
-       ('交通出行', 'system', 'expense'),
-       ('购物消费', 'system', 'expense'),
-       ('娱乐休闲', 'system', 'expense'),
-       ('生活缴费', 'system', 'expense'),
-       ('医疗健康', 'system', 'expense'),
-       ('人情往来', 'system', 'expense'),
-       ('教育学习', 'system', 'expense'),
-       ('工资奖金', 'system', 'income'),
-       ('补贴补助', 'system', 'income'),
-       ('退税退款', 'system', 'income'),
-       ('二手转卖', 'system', 'income'),
-       ('彩票收入', 'system', 'income'),
-       ('投资理财', 'system', 'income'),
-       ('借贷借款', 'system', 'income'),
-       ('五险一金', 'system', 'transfer'),
-       ('税费党费', 'system', 'transfer');
-
 -- 创建消费标签表
 CREATE TABLE IF NOT EXISTS tbl_billadm_tag
 (
@@ -64,55 +45,5 @@ CREATE TABLE IF NOT EXISTS tbl_billadm_tag
     category TEXT NOT NULL DEFAULT '',
     UNIQUE (name, scope, category)
 );
-
-INSERT INTO tbl_billadm_tag (name, scope, category)
-VALUES ('三餐', 'system', '餐饮美食'),
-       ('商场', 'system', '餐饮美食'),
-       ('外卖', 'system', '餐饮美食'),
-       ('奶茶', 'system', '餐饮美食'),
-       ('零食', 'system', '餐饮美食'),
-       ('水果', 'system', '餐饮美食'),
-       ('打车', 'system', '交通出行'),
-       ('地铁', 'system', '交通出行'),
-       ('公交', 'system', '交通出行'),
-       ('高铁', 'system', '交通出行'),
-       ('油费', 'system', '交通出行'),
-       ('停车', 'system', '交通出行'),
-       ('ETC', 'system', '交通出行'),
-       ('车险', 'system', '交通出行'),
-       ('衣物', 'system', '购物消费'),
-       ('数码', 'system', '购物消费'),
-       ('家居', 'system', '购物消费'),
-       ('书籍', 'system', '购物消费'),
-       ('礼物', 'system', '购物消费'),
-       ('玩具', 'system', '购物消费'),
-       ('宠物', 'system', '购物消费'),
-       ('游戏', 'system', '购物消费'),
-       ('快递', 'system', '购物消费'),
-       ('彩票', 'system', '购物消费'),
-       ('电影', 'system', '购物消费'),
-       ('健身', 'system', '购物消费'),
-       ('酒店', 'system', '购物消费'),
-       ('烟酒', 'system', '购物消费'),
-       ('充值', 'system', '购物消费'),
-       ('汽车', 'system', '购物消费'),
-       ('还款', 'system', '购物消费'),
-       ('房租', 'system', '生活缴费'),
-       ('物业', 'system', '生活缴费'),
-       ('水电', 'system', '生活缴费'),
-       ('燃气', 'system', '生活缴费'),
-       ('通讯', 'system', '生活缴费'),
-       ('人险', 'system', '生活缴费'),
-       ('还款', 'system', '生活缴费'),
-       ('医药', 'system', '医疗健康'),
-       ('医险', 'system', '医疗健康'),
-       ('红包', 'system', '人情往来'),
-       ('请客', 'system', '人情往来'),
-       ('养老', 'system', '五险一金'),
-       ('医疗', 'system', '五险一金'),
-       ('失业', 'system', '五险一金'),
-       ('住房', 'system', '五险一金'),
-       ('团费', 'system', '税费党费'),
-       ('交税', 'system', '税费党费');
 
 
