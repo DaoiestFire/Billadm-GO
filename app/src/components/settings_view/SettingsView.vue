b481ac8923de37999718aaf265bc8b729bde330f
ljw <liujingwei1997@outlook.com>
2026-01-12 22:36:11 +0800

    设置页面标签页增加图标

--- app/src/components/settings_view/SettingsView.vue
+++ app/src/components/settings_view/SettingsView.vue
@@ -1,14 +1,27 @@
 <template>
   <a-tabs v-model:activeKey="activeKey" style="height: 100%">
-    <a-tab-pane key="category&tag" tab="分类与标签">
+    <a-tab-pane key="category&tag">
+      <template #tab>
+        <span>
+          <TagOutlined style="font-size: medium"/>
+          分类与标签
+        </span>
+      </template>
       <billadm-category-tag-setting/>
     </a-tab-pane>
-    <a-tab-pane key="workspace" tab="工作空间">
+    <a-tab-pane key="workspace">
+      <template #tab>
+        <span>
+          <FolderOpenOutlined style="font-size: medium"/>
+          工作空间
+        </span>
+      </template>
     </a-tab-pane>
   </a-tabs>
 </template>
 <script lang="ts" setup>
 import {ref} from 'vue';
+import {FolderOpenOutlined, TagOutlined} from "@ant-design/icons-vue";
 
 const activeKey = ref('category&tag');
 </script>
